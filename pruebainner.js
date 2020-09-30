ad90a07d33e6f5968f93745c42fe99815cdc2f98
fiferortiz <pedrofelipeortiz@gmail.com>
2020-09-29 19:29:23 -0500

    arreglo tamaño cards

--- pruebainner.js
+++ pruebainner.js
@@ -30,12 +30,32 @@ const users = [
         gender: 'Female'
     },
     {
+        name: 'Antonia',
+        age: 35,
+        email: 'antonia@hotmail.com',
+        social: [
+            { name: 'facebook', url: 'facebook/antonia' },
+            { name: 'twitter', url: 'twitter/antonia' }
+        ],
+        gender: 'Female'
+    },
+    {
         name: 'Oscar',
         age: 31,
         email: 'oscar@academlo.com',
         social: [
             { name: 'facebook', url: 'facebook/oscar' },
             { name: 'twitter', url: 'twiter/oscar' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Felipe',
+        age: 28,
+        email: 'felipe@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/felipe' },
+            { name: 'twitter', url: 'twiter/felipe' }
         ],
         gender: 'Male'
     },
@@ -118,10 +138,10 @@ function generartablewomen(){
                     for (let i=0; i<users.length; i++){
                         let genero=users[i].gender;
                         if(genero=='Female'){
-                    const row ='<div class=".col-6 .col-md-4 .ml-3"><div class="card" style="width: 18rem;"><img id="imagencard" src="https://image.freepik.com/foto-gratis/color-rosa-electrico-abstraccion_23-2147734201.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">USUARIO'+i+'</h5><p class="card-text"> Nombre : '+users[i].name+'</p><p class="card-text">Email: '+users[i].email+'</p><p class="card-text"> Edad: '+users[i].age+'</p><p class="card-text"> Genero: '+users[i].gender+'</p><p class="card-text">Facebook: '+users[i].social[0].url+'</p><p class="card-text"Twitter :>'+users[i].social[1].url+'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>'
+                    const row ='<div class="col mb-4"><div class="card border-primary mb-3" style="width: 18rem;"><img id="imagencard" src="https://image.freepik.com/foto-gratis/color-rosa-electrico-abstraccion_23-2147734201.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">USUARIO'+i+'</h5><p class="card-text"> Nombre : '+users[i].name+'</p><p class="card-text">Email: '+users[i].email+'</p><p class="card-text"> Edad: '+users[i].age+'</p><p class="card-text"> Genero: '+users[i].gender+'</p><p class="card-text">Facebook: '+users[i].social[0].url+'</p><p class="card-text"Twitter :>'+users[i].social[1].url+'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>'
                     bodycard.innerHTML +=row;}
                         else{
-                            const row ='<div class=".col-6 .col-md-4 .ml-3"><div class="card" style="width: 18rem;"><img id="imagencard" src="https://image.freepik.com/foto-gratis/color-fondo-decorativo-azul-degradado-diagonal-textura-rayada_76263-671.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">USUARIO'+i+'</h5><p class="card-text"> Nombre : '+users[i].name+'</p><p class="card-text">Email: '+users[i].email+'</p><p class="card-text"> Edad: '+users[i].age+'</p><p class="card-text"> Genero: '+users[i].gender+'</p><p class="card-text">Facebook: '+users[i].social[0].url+'</p><p class="card-text"Twitter :>'+users[i].social[1].url+'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>'
+                            const row ='<div class="col mb-4"><div class="card" style="width: 18rem;"><img id="imagencard" src="https://image.freepik.com/foto-gratis/color-fondo-decorativo-azul-degradado-diagonal-textura-rayada_76263-671.jpg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">USUARIO'+i+'</h5><p class="card-text"> Nombre : '+users[i].name+'</p><p class="card-text">Email: '+users[i].email+'</p><p class="card-text"> Edad: '+users[i].age+'</p><p class="card-text"> Genero: '+users[i].gender+'</p><p class="card-text">Facebook: '+users[i].social[0].url+'</p><p class="card-text"Twitter :>'+users[i].social[1].url+'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>'
                             bodycard.innerHTML +=row;}  
                         }
                     
